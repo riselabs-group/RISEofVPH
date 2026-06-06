6fa24ba562bcd5ffee7c7a46fd48c22578a1b05b
rezishon <rezashahmoradi.81@outlook.com>
2026-06-06 21:01:10 +0330

    feat(riseofvph/main.py): add listener on "/start"
    By reciving the "/start" command, this function will be run.
    The response message also comes from a utils variable.

--- src/riseofvph/main.py
+++ src/riseofvph/main.py
@@ -1,5 +1,8 @@
 from deltachat2 import MsgData, events
 from deltabot_cli import BotCli
+from bot_handler.repo import send_msg
+from utils.repo import FIXED_RESPONSES
+
 cli = BotCli("echobot")
 
 
@@ -8,6 +11,9 @@ def log_event(bot, accid, event):
     bot.logger.info(event)
 
 
+@cli.on(events.NewMessage(command="/start"))
+def _start(bot, accid, event):
+    send_msg(bot, accid, event.msg.chat_id, FIXED_RESPONSES["/start"])
 
 
 if __name__ == "__main__":
